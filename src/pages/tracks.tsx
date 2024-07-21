e3ce86be576bb4fae08746ceb8fa3c90d29ddd21
Annie Huang <annie.hn@gmail.com>
2024-07-21 14:57:26 +1000

    Update tracks.tsx with GetTracks query.

--- src/pages/tracks.tsx
+++ src/pages/tracks.tsx
@@ -1,5 +1,24 @@
-import React from "react";
-import { Layout } from "../components";
+import React from 'react';
+import { Layout } from '../components';
+import { gql } from '../__generated__/';
+
+/** GET_TRACKS query to retrieve all tracks */
+const GET_TRACKS = gql(`
+  # Query goes here (keep it here to show the comment inside of gql)
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
