69989787321133d21fd69060db853bcd64cdec1c
Annie Huang <annie.hn@gmail.com>
2024-07-21 16:30:41 +1000

    Create "GET_TRACK" gql in track.tsx

--- src/pages/track.tsx
+++ src/pages/track.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 import { Layout, QueryResult } from '../components';
+import { gql } from '../__generated__';
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+
+export const GET_TRACK = gql(`
+  # our query goes here
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      description
+      numberOfViews
+      modules {
+        id
+        title
+        length
+        content
+        videoUrl
+      }
+    }
+  }
+`);
 
 const Track = () => {
+  const { trackId = '' } = useParams();
+
   return <Layout></Layout>;
 };
 
